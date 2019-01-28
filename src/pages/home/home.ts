3e5dcbd00ef49e16d7213cb5951bed837f657ba2
Asdruval De Leon <asdruval@DESKTOP-E02SJAT.localdomain>
2019-01-28 17:28:50 -0500

    http issue fixed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, AlertController } from "ionic-angular";
+import { TodoProvider } from "../../providers/todo/todo";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  public todos = [];
+  // public reorderIsEnabled = true;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    private todoProvider: TodoProvider,
+    public navCtrl: NavController,
+    private alertController: AlertController
+  ) {
+    this.todos = this.todoProvider.getTodos();
   }
 
+  openTodoAlert() {
+    let addTodoAlert = this.alertController.create({
+      title: "Add A Todo",
+      message: "Enter Your Todo",
+      inputs: [
+        {
+          type: "text",
+          name: "addTodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add Todo",
+          handler: inputData => {
+            let todoText;
+            todoText = inputData.addTodoInput;
+            // this.todos.push(todoText);
+            this.todoProvider.addTodos(todoText);
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
+  }
 }
---
+++ src/providers/todo/todo.ts
@@ -0,0 +1,24 @@
+import { Http } from "@angular/http";
+import { Injectable } from "@angular/core";
+
+/*
+  Generated class for the TodoProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class TodoProvider {
+  private todos = [];
+
+  constructor(public http: Http) {
+    console.log("Hello TodoProvider Provider");
+  }
+  getTodos() {
+    return this.todos;
+  }
+
+  addTodos(todo) {
+    this.todos.push(todo);
+  }
+}
